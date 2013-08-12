28a4fa20bfc5e232228be84226b90802924c1c8d
jayden <jayden@123-192-103-195.dynamic.kbronet.com.tw>
2013-08-12 23:04:03 +0800

    Dynamic Programming in bag problem

--- bagProblemInDP.c
+++ bagProblemInDP.c
@@ -6,11 +6,31 @@ int v[4] = {3,2,4,2};
 int n = 4;
 int limitWeight = 5; 
 int dfs(int,int,int);
-
+int dfsInDP(int,int);
 int main(void){
- printf("%d\n",dfs(0,0,0)); 
+ printf("%d\n",dfsInDP(0,0)); 
  return 0; 
 }
+
+int dfsInDP(int i,int sumWeight){
+  int sumOfAfterNodesValue=0;
+  int keep[100][100]={0};
+  if(keep[i][sumWeight]!=0){
+      return keep[i][sumWeight]; 
+  }
+  
+  if(i==4){
+     sumOfAfterNodesValue = 0;  
+  }else if(w[i]>limitWeight){
+     sumOfAfterNodesValue = dfsInDP(i+1,sumWeight);
+  }else if(sumWeight+w[i]<=limitWeight){
+     sumOfAfterNodesValue = max(dfsInDP(i+1,sumWeight),dfsInDP(i+1,sumWeight+w[i])+v[i]);
+  }else{
+     sumOfAfterNodesValue = dfsInDP(i+1,sumWeight);
+  }
+  return keep[i][sumWeight]=sumOfAfterNodesValue; 
+}
+
 
 int dfs(int i, int sumValue, int sumWeight){
   if(i==4){
